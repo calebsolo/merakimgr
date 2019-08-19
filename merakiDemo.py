f7697f5de78d10772541351b14e7dd9f7e5575f6
calebsolo <caleb.soileau@gmail.com>
2019-08-19 16:33:13 -0500

    edit to specify key

--- merakiDemo.py
+++ merakiDemo.py
@@ -1,8 +1,6 @@
 
 from meraki_sdk.meraki_sdk_client import MerakiSdkClient
 
-meraki_apikey = '15da0c6ffff295f16267f88f98694cf29a86ed87'  ##this is the demo key from Meraki
-client = MerakiSdkClient(meraki_apikey)
 
 def getOrgs():
 	orgs = client.organizations.get_organizations()
@@ -15,7 +13,8 @@ def getDeviceStatus(orgs):
 		for s in result:
 			print ("%s,%s" %(s['status'], s['serial']))
 	#return null
+meraki_apikey = input("Enter your API key:")#'15da0c6ffff295f16267f88f98694cf29a86ed87'  ##this is the demo key from Meraki
+client = MerakiSdkClient(meraki_apikey)
 
 orgsRtn = getOrgs()
 getDeviceStatus(orgsRtn)
-
